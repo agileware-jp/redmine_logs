0abdab0c939334b94a9728251828039b672d7e0b
fkdkent <fkdkent@agileware.jp>
2018-10-09 19:47:02 +0900

    Add copyright

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 # Logs plugin for Redmine
 # Copyright (C) 2010-2017  Haruyuki Iida
+# Copyright (C) 2018       Agileware Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
